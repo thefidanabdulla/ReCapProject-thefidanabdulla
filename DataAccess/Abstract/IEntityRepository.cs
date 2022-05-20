b0b7e814ab6943b448918bc795ea5856dcc566c6
thefidanabdulla <thefidanabdulla@gmail.com>
2022-05-20 16:10:16 +0400

    a little changes

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetById(T entity);
+    }
+}
