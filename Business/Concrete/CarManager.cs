53cb8c97831e66f77fbdfdff6c3da22087f8308b
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-11 18:30:38 +0400

    ValidationAspect added

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttinConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -22,12 +23,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
-        [Validate]
+        [ValidationAspect(typeof (CarValidator))]
         public IResult Add(Car car)
         {
-            
-            ValidationTool.Validate(new CarValidator(), car);
-
             _carDal.Add(car);
 
             return new SuccessResult(Messages.CarAdded);
