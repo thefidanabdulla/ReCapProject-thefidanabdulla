f24ea784798568c73f193c22e973c465d6ec711d
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-14 23:32:50 +0400

    CarImage added in Entity

---
+++ Entities/Concrete/CarImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class CarImage
+    {
+        public int CarImageId { get; set; }
+        public int CarId { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
