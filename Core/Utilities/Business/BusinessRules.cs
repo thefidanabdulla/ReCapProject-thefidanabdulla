da22daaf5dc62b3783779994d15b13f18241a0c5
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-18 21:02:33 +0400

    Camp 13. Day End

---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,22 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logigcs)
+        {
+            foreach (var logic in logigcs)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+            return null;
+        }
+    }
+}
