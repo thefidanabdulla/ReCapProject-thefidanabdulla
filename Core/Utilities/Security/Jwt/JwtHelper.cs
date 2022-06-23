bf659ee362e6a253838db6c3f22241c0af7a69c8
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-23 07:55:35 +0400

    Extentions added in Core

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,10 +1,12 @@
 ﻿using Core.Entities.Concrete;
+using Core.Extensions;
 using Core.Utilities.Security.Encryption;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 
 namespace Core.Utilities.Security.Jwt
