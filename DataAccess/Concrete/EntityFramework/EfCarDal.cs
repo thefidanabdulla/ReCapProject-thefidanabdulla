afa399b05917e505ec8767f5470314d5c63db072
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-02 15:23:47 +0400

    RentalDetailDto added succesfully

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -3,12 +3,8 @@ using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.Context;
 using Entities.Concrete;
 using Entities.DTOs;
-using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -2,13 +2,44 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.Context;
 using Entities.Concrete;
-using System;
+using Entities.DTOs;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfRentalDal:EfEntityRepositoryBase<Rental, CarContext>, IRentalDal
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, CarContext>, IRentalDal
     {
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            using (CarContext context = new CarContext())
+            {
+                var result = from ca in context.Cars
+                             join b in context.Brands
+                             on ca.BrandId equals b.Id
+                             join re in context.Rentals
+                             on ca.Id equals re.CarId
+                             join co in context.Colors
+                             on ca.ColorId equals co.Id
+                             from u in context.Users
+                             join cu in context.Customers
+                             on u.Id equals cu.UserId
+                             select new RentalDetailDto
+                             {
+                                 CarId = ca.Id,
+                                 BrandId = b.Id,
+                                 ColorName = co.ColorName,
+                                 BrandName = b.BrandName,
+                                 ModelName = ca.Description,
+                                 RentalId = re.Id,
+                                 RentDate = re.RentDate,
+                                 ReturnDate = re.ReturnDate,
+                                 CustomerName = u.FirstName,
+                                 CustomerLastname = u.LastName
+
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
