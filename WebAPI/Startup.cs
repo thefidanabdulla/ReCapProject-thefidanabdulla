0c4ddb1cb013f5d1db9f23c87a6a5e639bfc604d
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-02 21:02:14 +0400

    CORS added in WebAPI starup

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -38,6 +38,7 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -66,6 +67,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
